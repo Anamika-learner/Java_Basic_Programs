8b0a7b180e334318d05e3b49078eeebef6d5f01d
Anamika Rathod <arsibhai1922@gmail.com>
2025-04-13 11:17:10 +0530

    Delete 2.java

--- 2.java
+++
@@ -1,9 +0,0 @@
-public class onetofive {
-    public static void main(String[] args) {
-        int n = 5, i = 1;
-        while (i <= n) {
-            System.out.println(i);
-            i++;
-        }
-    }
-}
